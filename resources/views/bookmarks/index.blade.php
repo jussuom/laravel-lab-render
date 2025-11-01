3b4f12c2fd3f855f4245417226dfc9cdff5381d1
Jussi Suominen <jussuom.developer@gmail.com>
2025-11-01 18:29:44 +0200

    Show/hide "filter by category" section only if there are bookmarks

--- resources/views/bookmarks/index.blade.php
+++ resources/views/bookmarks/index.blade.php
@@ -5,25 +5,30 @@
         <h2 class="text-3xl mb-4">{{ __('Your Bookmarks') }}</h2>
         <hr class="my-4">
         {{-- <h3 class="text-center">{{ __('Filter by category') }}:</h3> --}}
-        <div class="flex justify-center flex-wrap my-4">
-            <div class="inline-block mx-2 my-1">
-                <a href="{{ route('bookmarks.index') }}"
-                    class="px-4 py-2 @if(!$categoryId) bg-gray-700 @endif text-white rounded inline-block">{{ __('All') }}</a>
-            </div>
-            @foreach ($categories as $category)
+        @if ($bookmarks->isEmpty() == false)
+            <div class="flex justify-center flex-wrap my-4">
+
                 <div class="inline-block mx-2 my-1">
-                    <a href="{{ route('bookmarks.index', ['category_id' => $category->id]) }}"
-                        class="px-4 py-2 @if(intval($categoryId) === $category->id) bg-gray-700 @endif text-white rounded inline-block">{{ $category->name }}</a>
+                    <a href="{{ route('bookmarks.index') }}"
+                        class="px-4 py-2 @if (!$categoryId) bg-gray-700 @endif text-white rounded inline-block">{{ __('All') }}</a>
                 </div>
-            @endforeach
-        </div>
-        <hr class="my-4">
-        @if ($bookmarks->isEmpty())
-            <p>{{ __("You don't have any bookmarks yet.") }}</p>
+                @foreach ($categories as $category)
+                    <div class="inline-block mx-2 my-1">
+                        <a href="{{ route('bookmarks.index', ['category_id' => $category->id]) }}"
+                            class="px-4 py-2 @if (intval($categoryId) === $category->id) bg-gray-700 @endif text-white rounded inline-block">{{ $category->name }}</a>
+                    </div>
+                @endforeach
+            </div>
             <hr class="my-4">
-            <p>
-                <a href="{{ route('bookmarks.create') }}" class="button">{{ __('Add Bookmark') }}</a>
-            </p>
+        @endif
+        @if ($bookmarks->isEmpty())
+            <div class="flex items-center my-8">
+                <div>{{ __("You don't have any bookmarks yet.") }}</div>
+                <div class="ml-4">
+                    <a href="{{ route('bookmarks.create') }}" class="button">{{ __('Add Bookmark') }}</a>
+                </div>
+            </div>
+            <hr class="my-4">
         @endif
         @foreach ($bookmarks as $bookmark)
             {{-- <x-bookmark :bookmark="$bookmark" :selectedCategoryIds="$categoryIds" /> --}}
